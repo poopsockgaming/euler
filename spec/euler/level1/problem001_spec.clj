f0e3cac3c0bf4835dc9f47524ea99a5adbe62c76
Caleb Mooi <calebmooi@Calebs-MacBook-Air.local>
2022-02-14 15:57:24 -0700

    solved 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,21 @@
 
 (describe "Euler Problem #1"
 
+  (it "naturals"
+    (should= [1 2 3 4 5 6 7 8 9] (naturals 10))
+    (should= [1 2 3 4] (naturals 5))
+    (should= [1 2] (naturals 3)))
+
+  (it "Multiples of 3 or 5"
+    (should= false (multiple-of-3-or-5? 1))
+    (should= false (multiple-of-3-or-5? 2))
+    (should= true (multiple-of-3-or-5? 3))
+    (should= false (multiple-of-3-or-5? 4))
+    (should= true (multiple-of-3-or-5? 5)))
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 233168 (euler-1 1000)))
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,12 @@
 (ns euler.level1.problem001)
 
+(defn naturals [x]
+  (rest (range x)))
+
+(defn multiple-of-3-or-5? [x]
+  (or (zero? (rem x 3))
+      (zero? (rem x 5))))
+
 (defn euler-1 [n]
+  (apply + (filter multiple-of-3-or-5? (naturals n)))
   )
